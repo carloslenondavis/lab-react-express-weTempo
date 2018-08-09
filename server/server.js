1cc8818442cdedf168865c31851eb1e9bd3fde30
Carlos Lenon <code@carloslenon.com>
2018-08-09 14:05:14 -0600

    remove morgan

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 import bodyParser from 'body-parser';
 //class
 import EndPoints from './endpoint';
@@ -7,7 +7,7 @@ import EndPoints from './endpoint';
 const app = express();
 const port = 5000;
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(EndPoints);
