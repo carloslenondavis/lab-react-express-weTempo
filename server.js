b9238f730f0a4eaf2aca591c6420212a81f2b2a9
Carlos Lenon <code@carloslenon.com>
2018-08-09 11:56:14 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/api/persons', (_req, _res)=>{
+    const persons = [
+        { id: 1, name:'nahara'},
+        { id: 2, name:'Sasha'},
+        { id: 3, name:'Lenon'},    
+    ]
+
+    _res.json(persons);
+})
+
+app.listen(port, () => console.log(`|o| yupiii!! your node api seed is now listening on port ${port}`));
